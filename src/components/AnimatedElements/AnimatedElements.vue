1efb5a6ca960b411e7fe91a38728b6d9682fc878
pawel lesniowski <pawel.html@gmail.com>
2019-12-07 19:41:40 +0100

    css aniamtaions

--- src/components/AnimatedElements/AnimatedElements.vue
+++ src/components/AnimatedElements/AnimatedElements.vue
@@ -4,13 +4,18 @@
     <transition name="fade">
       <p v-if="show">hello</p>
     </transition>
+    <hr />
+    <button @click="show2 = !show2">Toggle render</button>
+    <transition name="slide-fade">
+      <p v-if="show2">hello</p>
+    </transition>
   </div>
 </template>
 
 <script>
 export default {
   data: function() {
-    return { show: true };
+    return { show: true, show2: true };
   }
 };
 </script>
@@ -21,6 +26,18 @@ export default {
   transition: opacity 0.5s;
 }
 .fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
+}
+
+.slide-fade-enter-active {
+  transition: all 0.3s ease;
+}
+.slide-fade-leave-active {
+  transition: all 0.8s cubic-bezier(1, 0.5, 0.8, 1);
+}
+.slide-fade-enter, .slide-fade-leave-to
+/* .slide-fade-leave-active below version 2.1.8 */ {
+  transform: translateX(10px);
   opacity: 0;
 }
 </style>
